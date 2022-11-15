c22cc4698da271de6c19d452eee10fdf7c7ba5e7
Anas Mansy <anas393238@gmail.com>
2022-11-15 14:22:53 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,53 +1,44 @@
 import { Navbar, Nav, Col, Button, Container, Row } from 'react-bootstrap';
 import React, { Component, useState } from 'react'
 import { person } from './data';
+import FormIn from './component/FormIn';
+import QList from './component/QList';
+import { qustions } from './data';
 function App() {
-
-  const [display,setDisplay]=useState(true);
-  const handleDisplay=()=>{ 
-    setDisplay(true) 
+  const [data,setData]=useState(qustions);
+ 
+  const storageData=JSON.parse(localStorage.getItem("items")) 
+  const addItem=()=>{
+    localStorage.setItem("items",JSON.stringify([...qustions]))
+    setData([...qustions])
   }
-  const handleDelete=()=>{ 
-    setDisplay(false) 
+  const deleteAll=()=>{
+    if(localStorage.getItem("items")!=null){
+    qustions.splice(0,qustions.length)
+    localStorage.removeItem("items")}
+    setData([...qustions])
+  }
+  const deleteOne=()=>{
+    if(localStorage.getItem("items")!=null){
+    setData([...qustions])
+    localStorage.setItem("items",JSON.stringify([...qustions]))}
   }
   return (
     <div className="App">
       <Container className='py-5'>
-        <Row>
-          <Col sm="8" className="py-8">
-            {person.length ?
-              (<p>you hav a
-                <span style={{ color: 'red' }}> {person.length}</span> dates
-              </p>) : <p>no dates</p>}
+        <Row className='justify-content-center'> 
+        <Col sm="4" className="  text-center p-3 h4">
+            Q & A
+            </Col>  
+          <Col sm="8" className=" text-center">
+            <FormIn onAdd={addItem}/>
+            <QList data={data} deleteOne={deleteOne}/>
+            <Row className='px-2'>
+            {storageData?(<button className='btn btn-danger   mt-3  'onClick={deleteAll}>delete all</button>): null}  
+            </Row>
           </Col>
         </Row>
-        <Row>
-          <Col sm="8" className="py-8 my-2">
-            <div className='content p-2'>
-
-              {display&&
-                person.length ? (person.map((e) => {
-                  return <>
-                    <div className='d-flex item my-2'>
-                      <img className='avatar' src={e.img} />
-                      <div>
-                        <p>{e.name}</p>
-                        <p>{e.date}</p>
-                      </div>
-                    </div>
-                  </>
-                })) : null
-              }
-
-            </div>
-          </Col>
-        </Row>
-        <Row>
-          <Col sm="8" className="py-8 d-flex justify-content-between  p-5">
-            <Button className='btn btn-danger px-5' onClick={handleDelete}>delete all</Button>
-            <Button className='btn btn-primary px-5'  onClick={handleDisplay}>display all dates</Button>
-          </Col>
-        </Row>
+        
       </Container>
     </div>
   );
